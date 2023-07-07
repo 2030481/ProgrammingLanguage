2a0cc79b23c86b22d11685370ce7abef4929eb4e
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2023-07-07 23:47:09 +0530

    CreateBinaryArithmeticOperatorinC.c

---
+++ BinaryArithmeticOperatorinC.c
@@ -0,0 +1,28 @@
+// C program to demonstrate syntax of binary arithmetic
+// operators
+#include <stdio.h>
+
+int main()
+{
+	int a = 10, b = 4, res;
+
+	// printing a and b
+	printf("a is %d and b is %d\n", a, b);
+
+	res = a + b; // addition
+	printf("a + b is %d\n", res);
+
+	res = a - b; // subtraction
+	printf("a - b is %d\n", res);
+
+	res = a * b; // multiplication
+	printf("a * b is %d\n", res);
+
+	res = a / b; // division
+	printf("a / b is %d\n", res);
+
+	res = a % b; // modulus
+	printf("a %% b is %d\n", res);
+
+	return 0;
+}
