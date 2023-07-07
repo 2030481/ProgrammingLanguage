7e62d8be92d7402f60477a8dad6f2d8f5a6e5c1c
Ravi Ranjan Kumar <92367233+2030481@users.noreply.github.com>
2023-07-07 23:45:55 +0530

    Create Scanf.c

---
+++ Scanf.c
@@ -0,0 +1,20 @@
+// C program to implement
+// scanf
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+	int a, b;
+
+	printf("Enter first number: ");
+	scanf("%d", &a);
+
+	printf("Enter second number: ");
+	scanf("%d", &b);
+
+	printf("A : %d \t B : %d" ,
+			a , b);
+
+	return 0;
+}
